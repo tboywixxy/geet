ab0d9b21b9ad5b330adaff865e5f58dfaa6b0b95
tboywixxy <tboywixxy1@gmail.com>
2025-12-07 19:34:25 +0100

    Update Next.js/React

--- src/app/gallery/albums/crescendoIkeja.ts
+++ src/app/gallery/albums/crescendoIkeja.ts
@@ -4,55 +4,55 @@ import type { GalleryItem } from "../albumsData";
 // Gallery items (images) for Cloudinary folder "Eighty5ers/Crescendo Ikeja"
 export const CRESCENDO_IKEJA_IMAGES: GalleryItem[] = [
   {
-    type: "image",
+    
     src: "https://res.cloudinary.com/df2e1ug1q/image/upload/v1765093927/IMG-20150629-WA0005_cnysr3.jpg",
     alt: "Crescendo Ikeja 1",
     label: "Crescendo Ikeja 1",
   },
   {
-    type: "image",
+    
     src: "https://res.cloudinary.com/df2e1ug1q/image/upload/v1765093927/IMG-20150629-WA0006_pp6mql.jpg",
     alt: "Crescendo Ikeja 2",
     label: "Crescendo Ikeja 2",
   },
   {
-    type: "image",
+    
     src: "https://res.cloudinary.com/df2e1ug1q/image/upload/v1765093927/IMG-20150629-WA0007_t1j0bt.jpg",
     alt: "Crescendo Ikeja 3",
     label: "Crescendo Ikeja 3",
   },
   {
-    type: "image",
+    
     src: "https://res.cloudinary.com/df2e1ug1q/image/upload/v1765093927/IMG-20150629-WA0008_bq0j7k.jpg",
     alt: "Crescendo Ikeja 4",
     label: "Crescendo Ikeja 4",
   },
   {
-    type: "image",
+    
     src: "https://res.cloudinary.com/df2e1ug1q/image/upload/v1765093927/IMG-20150629-WA0009_nxvlem.jpg",
     alt: "Crescendo Ikeja 5",
     label: "Crescendo Ikeja 5",
   },
   {
-    type: "image",
+    
     src: "https://res.cloudinary.com/df2e1ug1q/image/upload/v1765093928/IMG-20150629-WA0010_wcopx8.jpg",
     alt: "Crescendo Ikeja 6",
     label: "Crescendo Ikeja 6",
   },
   {
-    type: "image",
+    
     src: "https://res.cloudinary.com/df2e1ug1q/image/upload/v1765093928/IMG-20150629-WA0011_rnax6v.jpg",
     alt: "Crescendo Ikeja 7",
     label: "Crescendo Ikeja 7",
   },
   {
-    type: "image",
+    
     src: "https://res.cloudinary.com/df2e1ug1q/image/upload/v1765093928/IMG-20150808-WA0025_tjmofp.jpg",
     alt: "Crescendo Ikeja 8",
     label: "Crescendo Ikeja 8",
   },
   {
-    type: "image",
+    
     src: "https://res.cloudinary.com/df2e1ug1q/image/upload/v1765093928/IMG-20171024-WA0042_bwrofi.jpg",
     alt: "Crescendo Ikeja 9",
     label: "Crescendo Ikeja 9",
--- src/app/gallery/albums/principalStaff.ts
+++ src/app/gallery/albums/principalStaff.ts
@@ -4,55 +4,55 @@ import type { GalleryItem } from "../albumsData";
 // Gallery items (images) for Cloudinary folder "Eighty5ers/Some Pix of Principal and Staff"
 export const PRINCIPAL_STAFF_IMAGES: GalleryItem[] = [
   {
-    type: "image",
+    
     src: "https://res.cloudinary.com/df2e1ug1q/image/upload/v1765093925/IMG-20150722-WA0000_azhcyj.jpg",
     alt: "Some Pix of Principal and Staff 1",
     label: "Some Pix of Principal and Staff 1",
   },
   {
-    type: "image",
+    
     src: "https://res.cloudinary.com/df2e1ug1q/image/upload/v1765093925/IMG-20170128-WA0001_q0pjww.jpg",
     alt: "Some Pix of Principal and Staff 2",
     label: "Some Pix of Principal and Staff 2",
   },
   {
-    type: "image",
+    
     src: "https://res.cloudinary.com/df2e1ug1q/image/upload/v1765093926/IMG-20171019-WA0026_swtq2q.jpg",
     alt: "Some Pix of Principal and Staff 3",
     label: "Some Pix of Principal and Staff 3",
   },
   {
-    type: "image",
+    
     src: "https://res.cloudinary.com/df2e1ug1q/image/upload/v1765093926/IMG-20171019-WA0027_xoelru.jpg",
     alt: "Some Pix of Principal and Staff 4",
     label: "Some Pix of Principal and Staff 4",
   },
   {
-    type: "image",
+    
     src: "https://res.cloudinary.com/df2e1ug1q/image/upload/v1765093926/IMG-20171019-WA0029_grpyem.jpg",
     alt: "Some Pix of Principal and Staff 5",
     label: "Some Pix of Principal and Staff 5",
   },
   {
-    type: "image",
+    
     src: "https://res.cloudinary.com/df2e1ug1q/image/upload/v1765093926/IMG-20171019-WA0038_itkzcs.jpg",
     alt: "Some Pix of Principal and Staff 6",
     label: "Some Pix of Principal and Staff 6",
   },
   {
-    type: "image",
+    
     src: "https://res.cloudinary.com/df2e1ug1q/image/upload/v1765093926/IMG-20171019-WA0040_tisxrj.jpg",
     alt: "Some Pix of Principal and Staff 7",
     label: "Some Pix of Principal and Staff 7",
   },
   {
-    type: "image",
+    
     src: "https://res.cloudinary.com/df2e1ug1q/image/upload/v1765093927/IMG-20171021-WA0004_aiqvq8.jpg",
     alt: "Some Pix of Principal and Staff 8",
     label: "Some Pix of Principal and Staff 8",
   },
   {
-    type: "image",
+    
     src: "https://res.cloudinary.com/df2e1ug1q/image/upload/v1765093927/IMG-20171021-WA0019_nogkz1.jpg",
     alt: "Some Pix of Principal and Staff 9",
     label: "Some Pix of Principal and Staff 9",
